8f0382e7bdca530c89882821ae123453b2643695
rocky-co <65335475+rocky-co@users.noreply.github.com>
2020-05-14 11:41:37 +0000

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+username = prompt('用户名：')
+password = prompt('密码：')
+if (username = 'admin'){
+alert('用户名正确：admin，正在解析密码...')
+} elif (password == 'admin'){
+alert('登录完成！管理员你好~')
+
+}
+}
